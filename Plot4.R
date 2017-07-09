2bc7c9bd5d7fd3f38d811f9a46a8ae6a1e86a9d1
Isabel Fernandez-Gomez <isabelfg79@gmail.com>
2017-07-09 20:58:23 +0200

    Update Plot4.R

--- Plot4.R
+++ Plot4.R
@@ -18,29 +18,27 @@ my_data$Datetime <- as.POSIXct(datetime)
 # Create a window with 2 rows and 2 col
 par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
 
+# Plot the 4 
 with(my_data, {
-  
   plot(Global_active_power ~ Datetime, 
        type="l", 
        ylab="Global Active Power (kW)")
-  
   plot(Voltage ~ Datetime, 
        type="l", 
        ylab="Voltage (volt)")
-  
   plot(Sub_metering_1 ~ Datetime, 
        type="l", 
        ylab="Global Active Power (kW)")
-  
   lines(Sub_metering_2 ~ Datetime,col='Red')
-  
   lines(Sub_metering_3 ~ Datetime,col='Blue')
-  
   legend("topright", 
          col=c("black", "red", "blue"), 
          lty=1, lwd=2, bty="n",
          legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-  
   plot(Global_reactive_power ~ Datetime, 
        type="l", 
        ylab="Global Rective Power (kW)",xlab="")})
+
+# Save to png file
+dev.copy(png, "plot4.png", width=480, height=480)
+dev.off()
