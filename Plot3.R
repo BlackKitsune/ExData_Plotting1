08c4974176d382ed733ecde50f893b5cc31ead42
Isabel Fernandez-Gomez <isabelfg79@gmail.com>
2017-07-09 20:54:10 +0200

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -21,11 +21,12 @@ with(my_data, {plot(Sub_metering_1 ~ Datetime,
                  lines(Sub_metering_2 ~ Datetime,col='Red')
                  lines(Sub_metering_3 ~ Datetime,col='Blue')})
 
-
+# Add legend
 legend("topright", 
        col=c("black", "red", "blue"), 
        lty=1, lwd=2, 
        legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
+# Save to png file format
 dev.copy(png, "plot3.png", width=480, height=480)
 dev.off()
